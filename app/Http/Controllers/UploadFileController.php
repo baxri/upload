7dc2b75e3152942fab90a9eaf58357c34f8eabc6
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-05-17 16:06:32 +0400

    --

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -8,6 +8,10 @@ class UploadFileController extends Controller
 {
     public function upload(Request $request)
     {
+        $dbfile = $request->file('dbfile');
 
+        //Move Uploaded File
+        $destinationPath = 'uploads';
+        $dbfile->move($destinationPath,$dbfile->getClientOriginalName());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/upload', 'UploadFileController@upload');
+
