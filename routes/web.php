eb453fdfde20cb1d8ada8ee788e7526cbbe19599
giorgi <giorgi.bibilashvili89@gmail.com>
2018-06-03 01:53:01 +0400

    --

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,20 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
+//http://upload.local/admin/download/1
+
 Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
     CRUD::resource('upload', 'Admin\UploadCrudController');
-    Route::get('download/{upload}', 'Admin\UploadCrudController@download');
+//    Route::get('download/{upload}', 'Admin\UploadCrudController@download');
 
     Route::group(['prefix' => 'upload/{parent_id}'], function () {
         CRUD::resource('images', 'Admin\ImageCrudController');
     });
 
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('download/{upload}', 'Admin\UploadCrudController@download');
 });
 
 
