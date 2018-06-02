80406cfa6bec80928fdc4c52ae4bf0075470e820
Mac <mac@Macs-MacBook-Pro.local>
2018-06-02 13:58:55 +0400

    --

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Upload;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
 
 class UploadFileController extends Controller
 {
