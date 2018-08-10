3ae7aa24bade079fcf4be62f0ec49a5df3fb0cdc
giorgi <giorgi.bibilashvili89@gmail.com>
2018-08-10 22:15:24 +0400

    --

---
+++ database/migrations/2018_08_10_180749_add_admin_password_to_uploads_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminPasswordToUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('uploads', function ($table) {
+            $table->string('admin-password')->after('filename')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
