99348a061b944a42f718b9551d218a0e95f92ed5
giorgi <giorgi.bibilashvili89@gmail.com>
2018-11-24 07:14:17 +0400

    --

--- app/Http/Controllers/BackupControler.php
+++ app/Http/Controllers/BackupControler.php
@@ -27,7 +27,10 @@ class BackupControler extends Controller
 //        $url = 'http://178.128.246.80:8080?document=Controle&realm_path=../haccp/public/uploads/375a3892440333de-1537388138/haccp-db-8.realm';
         $url = base64_decode($url);
         $output = file_get_contents($url);
-        return $output;
+        return [
+            'total' => count($output),
+            'list' => $output,
+        ];
     }
 
 }
