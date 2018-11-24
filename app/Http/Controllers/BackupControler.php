cbaceaa8b8ca75fe0210f825cc0635b848dccd4e
giorgi <giorgi.bibilashvili89@gmail.com>
2018-11-24 07:10:56 +0400

    --

--- app/Http/Controllers/BackupControler.php
+++ app/Http/Controllers/BackupControler.php
@@ -26,12 +26,7 @@ class BackupControler extends Controller
         $url = $request->input('url');
 //        $url = 'http://178.128.246.80:8080?document=Controle&realm_path=../haccp/public/uploads/375a3892440333de-1537388138/haccp-db-8.realm';
         $url = base64_decode($url);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, "My User Agent Name");
-        $output = curl_exec($ch);
-        curl_close($ch);
+        $output = file_get_contents($url);
         return $output;
     }
 
