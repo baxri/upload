e2bb4aac7a6532fb0dae1c996ea758353de2988a
giorgi <giorgi.bibilashvili89@gmail.com>
2019-03-10 01:02:41 +0400

    --

--- app/Upload.php
+++ app/Upload.php
@@ -28,7 +28,6 @@ class Upload extends Model
         return '<a href="upload/' . $this->id . '/images">Images (' . count($this->images) . ')</a>';
     }
 
-
     public function getDetailsLink()
     {
         return '<a href="#">Open Backup</a>';
